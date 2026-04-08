588cf0eb6bb25af65a8f02111e8baa36051ff2bc
Stephanie Pelham <stephaniepelham@dyn242-229.wireless-1725.ndsu.NoDak.edu>
2026-04-08 12:49:34 -0500

    add RouterLink import to fix navigation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, RouterLink, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
