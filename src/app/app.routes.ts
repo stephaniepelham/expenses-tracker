4469846109dccd15f92444cbf59952b3f9530288
Stephanie Pelham <stephaniepelham@dyn242-229.wireless-1725.ndsu.NoDak.edu>
2026-04-08 12:45:45 -0500

    fix routing and component imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,10 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard';
 import { AddExpenseComponent } from './components/add-expense/add-expense';
 import { ExpenseListComponent } from './components/expense-list/expense-list';
 
 export const routes: Routes = [
-{ path: '', component: DashboardComponent },
-{ path: 'add', component: AddExpenseComponent },
-{ path: 'expenses', component: ExpenseListComponent }
+  { path: '', component: DashboardComponent },
+  { path: 'add', component: AddExpenseComponent },
+  { path: 'expenses', component: ExpenseListComponent }
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule {}
--- src/app/components/expense-item/expense-item.ts
+++ src/app/components/expense-item/expense-item.ts
@@ -1,9 +1,11 @@
 import { Component, Input, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Expense } from '../../models/expense';
 import { ExpenseService } from '../../services/expense-service';
 
 @Component({
   selector: 'app-expense-item',
+  imports: [CommonModule],
   templateUrl: './expense-item.html'
 })
 export class ExpenseItemComponent {
